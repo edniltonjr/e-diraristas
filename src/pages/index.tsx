3db3fa64c621a6c09cbf27303fa09881ee98e762
Ednilton Junior <jrjunior_cg@live.com>
2021-06-12 02:42:30 -0300

    Structure Body Informations

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,21 @@
-import Head from 'next/head';
-import SafeEnviroment from 'ui/components/feedback/SafeEnviroment';
-import PageTitle from 'ui/components/data-display/PageTitle';
+import SafeEnviroment from "ui/components/feedback/SafeEnviroment";
+import PageTitle from "ui/components/data-display/PageTitle";
+import UserInformation from "ui/components/data-display/UserInformation";
 
 export default function Home() {
   return (
     <div>
-      <SafeEnviroment/> 
-      <PageTitle 
+      <SafeEnviroment />
+      <PageTitle
         title="Conheça os profissionais"
-        subtitle="Preencha seu endereço e veja todos os profissionais da sua localidade"/>
-  
-
+        subtitle="Preencha seu endereço e veja todos os profissionais da sua localidade"
+      />
+      <UserInformation
+        name={"Sanira Tamada"}
+        picture={"https://avatars.githubusercontent.com/u/24655967?v=4"}
+        rating={3}
+        description={"Campina Grande"}
+      />
     </div>
-  )
+  );
 }
